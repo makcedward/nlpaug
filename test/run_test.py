fd52dd6a01f089654e90643810130d72ac576fe9
Edward Ma <makcedward@gmail.com>
2019-09-03 06:26:37 -0700

    Update readme

--- test/run_test.py
+++ test/run_test.py
@@ -1,10 +1,14 @@
 import unittest
+import sys
 
 
 if __name__ == '__main__':
+    sys.path.append('../nlpaug')
+
     test_dirs = [
         'test/augmenter/char/',
         'test/augmenter/word/',
+        'test/augmenter/sentence/',
         'test/augmenter/audio/',
         'test/augmenter/spectrogram/',
         'test/flow/'
@@ -17,5 +21,8 @@ if __name__ == '__main__':
        suite = loader.discover(test_dir)
        runner.run(suite)
 
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.word.test_wordembs')
+    # suite = unittest.TestLoader().loadTestsFromName('augmenter.sentence.test_context_word_embs_sentence')
+    # runner.run(suite)
+
+    # suite = unittest.TestLoader().loadTestsFromName('augmenter.word.test_context_word_embs')
     # runner.run(suite)
