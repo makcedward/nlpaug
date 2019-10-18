2d1c4da1029cd111dc027ff135faf45aaae107ac
Edward Ma <makcedward@gmail.com>
2019-10-17 21:39:34 -0700

    Introduce random word subsitute

--- test/augmenter/word/test_random_word.py
+++ test/augmenter/word/test_random_word.py
@@ -12,12 +12,38 @@ class TestRandom(unittest.TestCase):
         aug = naw.RandomWordAug(action=Action.SWAP)
 
         for text in texts:
-            self.assertLess(0, len(text))
             augmented_text = aug.augment(text)
-
             self.assertNotEqual(text, augmented_text)
 
-        self.assertLess(0, len(texts))
+    def test_substitute_without_target_word(self):
+        texts = [
+            'The quick brown fox jumps over the lazy dog'
+        ]
+        aug = naw.RandomWordAug(action='substitute')
+
+        for text in texts:
+            augmented_text = aug.augment(text)
+
+            self.assertIn('_', augmented_text)
+            self.assertNotEqual(text, augmented_text)
+
+    def test_substitute_with_target_word(self):
+        texts = [
+            'The quick brown fox jumps over the lazy dog'
+        ]
+        target_words = ['$', '#', '^^^']
+        aug = naw.RandomWordAug(action='substitute', target_words=target_words)
+
+        for text in texts:
+            augmented_text = aug.augment(text)
+
+            replaced = False
+            for w in target_words:
+                if w in augmented_text:
+                    replaced = True
+                    break
+            self.assertTrue(replaced)
+            self.assertNotEqual(text, augmented_text)
 
     def test_delete(self):
         texts = [
@@ -26,9 +52,5 @@ class TestRandom(unittest.TestCase):
         aug = naw.RandomWordAug()
 
         for text in texts:
-            self.assertLess(0, len(text))
             augmented_text = aug.augment(text)
-
             self.assertNotEqual(text, augmented_text)
-
-        self.assertLess(0, len(texts))
