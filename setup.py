3368deb1db1c4495d886abff3f071aefc6a0dc6d
Edward Ma <makcedward@gmail.com>
2020-08-21 21:24:00 -0700

    release 0.0.18

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", encoding="utf8") as f:
 
 setup(
     name="nlpaug",
-    version="0.0.17",
+    version="0.0.18",
     author="Edward Ma",
     author_email="makcedward@gmail.com",
     url="https://github.com/makcedward/nlpaug",
