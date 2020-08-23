6951ade3751b46757170c4f7a13b616fe2decfc5
Edward Ma <makcedward@gmail.com>
2020-08-22 17:33:07 -0700

    prepare for 1.0.0dev

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", encoding="utf8") as f:
 
 setup(
     name="nlpaug",
-    version="0.0.18",
+    version="1.0.0dev",
     author="Edward Ma",
     author_email="makcedward@gmail.com",
     url="https://github.com/makcedward/nlpaug",
