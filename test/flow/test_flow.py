fb9a4ccf4ee832e0dcc9f968aadabcdda7292a12
Edward Ma <makcedward@gmail.com>
2020-09-19 07:18:11 -0700

    test cases adopt new interface

--- test/flow/test_flow.py
+++ test/flow/test_flow.py
@@ -23,246 +23,249 @@ class TestFlow(unittest.TestCase):
             os.environ.get("TEST_DIR"), 'res', 'audio', 'Yamaha-V50-Rock-Beat-120bpm.wav'
         )
 
-    # def test_dry_run(self):
-    #     flow = naf.Sequential([naf.Sequential()])
-    #     results = flow.augment([])
-    #     self.assertEqual(0, len(results))
-    #
-    # def test_multiple_actions(self):
-    #     texts = [
-    #         'The quick brown fox jumps over the lazy dog',
-    #         'Zology raku123456 fasdasd asd4123414 1234584'
-    #     ]
-    #
-    #     flows = [
-    #         naf.Sequential([
-    #             naf.Sometimes([nac.RandomCharAug(action="insert"),
-    #                            nac.RandomCharAug(action="delete")],
-    #                           pipeline_p=0.9),
-    #             naf.Sequential([
-    #                 nac.RandomCharAug(action="substitute", aug_char_min=1, aug_char_p=0.6, aug_word_p=0.6)
-    #             ], name='Sub_Seq')
-    #         ]),
-    #         naf.Sometimes([
-    #             naf.Sometimes([nac.RandomCharAug(action="insert"),
-    #                            nac.RandomCharAug(action="delete")]),
-    #             naf.Sequential([nac.OcrAug(), nac.KeyboardAug(aug_char_min=1),
-    #                             nac.RandomCharAug(action="substitute", aug_char_min=1, aug_char_p=0.6, aug_word_p=0.6)])
-    #         ], pipeline_p=0.9)
-    #     ]
-    #
-    #     # Since prob may be low and causing do not perform data augmentation. Retry 5 times
-    #     for flow in flows:
-    #         for text in texts:
-    #             at_least_one_not_equal = False
-    #             for _ in range(5):
-    #                 augmented_text = flow.augment(text, n=1)
-    #
-    #                 if text != augmented_text:
-    #                     at_least_one_not_equal = True
-    #                     break
-    #
-    #             self.assertTrue(at_least_one_not_equal)
-    #     self.assertLess(0, len(flows))
-    #     self.assertLess(0, len(texts))
-    #
-    # def test_n_output_textual(self):
-    #     texts = [
-    #         'The quick brown fox jumps over the lazy dog',
-    #         'Zology raku123456 fasdasd asd4123414 1234584',
-    #         'AAAAAAAAAAA AAAAAAAAAAAAAA'
-    #     ]
-    #     flows = [
-    #         naf.Sequential([
-    #             nac.RandomCharAug(action="insert"),
-    #             naw.RandomWordAug()
-    #         ]),
-    #         naf.Sometimes([
-    #             nac.RandomCharAug(action="insert"),
-    #             nac.RandomCharAug(action="delete")
-    #         ], pipeline_p=0.9),
-    #         naf.Sequential([
-    #             naf.Sequential([
-    #                 nac.RandomCharAug(action="insert"),
-    #                 naw.RandomWordAug()
-    #             ]),
-    #             naf.Sometimes([
-    #                 nac.RandomCharAug(action="insert"),
-    #                 nac.RandomCharAug(action="delete")
-    #             ], pipeline_p=0.9)
-    #         ])
-    #     ]
-    #
-    #     for flow in flows:
-    #         for text in texts:
-    #             augmented_texts = flow.augment(text, n=3)
-    #             self.assertGreater(len(augmented_texts), 1)
-    #             for augmented_text in augmented_texts:
-    #                 self.assertNotEqual(augmented_text, text)
-    #
-    #     self.assertLess(0, len(flows))
-    #     self.assertLess(0, len(texts))
-    #
-    # def test_n_output_audio(self):
-    #     audio, sampling_rate = AudioLoader.load_audio(self.sample_wav_file)
-    #
-    #     flows = [
-    #         naf.Sequential([
-    #             naa.CropAug(sampling_rate=sampling_rate),
-    #             naa.LoudnessAug()
-    #         ]),
-    #         naf.Sometimes([
-    #             naa.CropAug(sampling_rate=sampling_rate),
-    #             naa.LoudnessAug()
-    #         ], pipeline_p=0.9),
-    #         naf.Sequential([
-    #             naf.Sequential([
-    #                 naa.CropAug(sampling_rate=sampling_rate),
-    #                 naa.LoudnessAug()
-    #             ]),
-    #             naf.Sometimes([
-    #                 naa.CropAug(sampling_rate=sampling_rate),
-    #                 naa.LoudnessAug()
-    #             ], pipeline_p=0.9)
-    #         ])
-    #     ]
-    #
-    #     for flow in flows:
-    #         augmented_audios = flow.augment(audio, n=3)
-    #         self.assertGreater(len(augmented_audios), 1)
-    #         for augmented_audio in augmented_audios:
-    #             self.assertFalse(np.array_equal(audio, augmented_audio))
-    #
-    #     self.assertLess(0, len(flows))
-    #
-    # def test_n_output_spectrogram(self):
-    #     mel_spectrogram = AudioLoader.load_mel_spectrogram(self.sample_wav_file, n_mels=128)
-    # #
-    #     flows = [
-    #         naf.Sequential([
-    #             nas.FrequencyMaskingAug(mask_factor=80),
-    #             nas.TimeMaskingAug(mask_factor=80)
-    #         ]),
-    #         naf.Sometimes([
-    #             nas.FrequencyMaskingAug(mask_factor=80),
-    #             nas.TimeMaskingAug(mask_factor=80)
-    #         ], pipeline_p=0.9),
-    #         naf.Sequential([
-    #             naf.Sequential([
-    #                 nas.FrequencyMaskingAug(mask_factor=80),
-    #                 nas.TimeMaskingAug(mask_factor=80)
-    #             ]),
-    #             naf.Sometimes([
-    #                 nas.FrequencyMaskingAug(mask_factor=80),
-    #                 nas.TimeMaskingAug(mask_factor=80)
-    #             ], pipeline_p=0.9)
-    #         ])
-    #     ]
-    #
-    #     for flow in flows:
-    #         augmented_mel_spectrograms = flow.augment(mel_spectrogram, n=3)
-    #         self.assertGreater(len(augmented_mel_spectrograms), 1)
-    #         for augmented_mel_spectrogram in augmented_mel_spectrograms:
-    #             self.assertFalse(np.array_equal(mel_spectrogram, augmented_mel_spectrogram))
-    #
-    #     self.assertLess(0, len(flows))
-    #
-    # def test_n_output_without_augmentation(self):
-    #     texts = [
-    #         'AAAAAAAAAAA AAAAAAAAAAAAAA'
-    #     ]
-    #     flows = [
-    #         naf.Sequential([
-    #             nac.OcrAug(),
-    #             nac.OcrAug()
-    #         ]),
-    #         naf.Sometimes([
-    #             nac.RandomCharAug(),
-    #             nac.RandomCharAug()
-    #         ], pipeline_p=0.00001)
-    #     ]
-    #
-    #     for flow in flows:
-    #         for text in texts:
-    #             for _ in range(5):
-    #                 augmented_texts = flow.augment(text, n=3)
-    #                 all_not_equal = False
-    #                 for augmented_text in augmented_texts:
-    #                     if augmented_text != text:
-    #                         all_not_equal = True
-    #                         break
-    #                 if all_not_equal:
-    #                     break
-    #
-    #             self.assertFalse(all_not_equal)
-    #     self.assertLess(0, len(flows))
-    #     self.assertLess(0, len(texts))
-    #
-    # def test_multi_thread(self):
-    #     text = 'The quick brown fox jumps over the lazy dog'
-    #     n = 3
-    #     flows = [
-    #         naf.Sequential([
-    #             naf.Sequential([
-    #                 nac.OcrAug(),
-    #                 naw.WordEmbsAug(
-    #                     model_type='word2vec',
-    #                     model_path=os.environ["MODEL_DIR"] + 'GoogleNews-vectors-negative300.bin')
-    #             ]),
-    #             naf.Sequential([
-    #                 nac.RandomCharAug(),
-    #             ]),
-    #             naw.ContextualWordEmbsAug(
-    #                 model_path='xlnet-base-cased', action="substitute",
-    #                 skip_unknown_word=True, temperature=0.7, device='cpu')
-    #         ]),
-    #         naf.Sometimes([
-    #             naf.Sequential([
-    #                 nac.OcrAug(),
-    #                 nac.RandomCharAug(),
-    #             ]),
-    #             naf.Sometimes([
-    #                 naw.WordEmbsAug(model_type='word2vec',
-    #                                 model_path=os.environ["MODEL_DIR"] + 'GoogleNews-vectors-negative300.bin')
-    #             ], pipeline_p=0.999),
-    #             naw.ContextualWordEmbsAug(
-    #                 model_path='xlnet-base-cased', action="substitute",
-    #                 skip_unknown_word=True, temperature=0.7, device='cpu')
-    #         ], pipeline_p=0.9999)
-    #     ]
-    #
-    #     for num_thread in [1, 3]:
-    #         for flow in flows:
-    #             augmented_data = flow.augment(text, n=n, num_thread=num_thread)
-    #             self.assertEqual(len(augmented_data), n)
-
-    def test_augment_detail(self):
-        text = 'The quick brown fox jumps over the lazy dog'
-
+    def test_dry_run(self):
+        flow = naf.Sequential([naf.Sequential()])
+        results = flow.augment([])
+        self.assertEqual(0, len(results))
+    
+    def test_multiple_actions(self):
+        texts = [
+            'The quick brown fox jumps over the lazy dog',
+            'Zology raku123456 fasdasd asd4123414 1234584'
+        ]
+    
         flows = [
             naf.Sequential([
                 naf.Sometimes([nac.RandomCharAug(action="insert"),
                                nac.RandomCharAug(action="delete")],
-                              pipeline_p=0.5),
+                              pipeline_p=0.9),
                 naf.Sequential([
                     nac.RandomCharAug(action="substitute", aug_char_min=1, aug_char_p=0.6, aug_word_p=0.6)
                 ], name='Sub_Seq')
-            ], include_detail=True),
+            ]),
             naf.Sometimes([
                 naf.Sometimes([nac.RandomCharAug(action="insert"),
                                nac.RandomCharAug(action="delete")]),
                 naf.Sequential([nac.OcrAug(), nac.KeyboardAug(aug_char_min=1),
                                 nac.RandomCharAug(action="substitute", aug_char_min=1, aug_char_p=0.6, aug_word_p=0.6)])
-            ], pipeline_p=1, include_detail=True)
+            ], pipeline_p=0.9)
         ]
-
+    
+        # Since prob may be low and causing do not perform data augmentation. Retry 5 times
         for flow in flows:
-            augmented_text, augment_details = flow.augment(text)
+            for text in texts:
+                at_least_one_not_equal = False
+                for _ in range(5):
+                    augmented_text = flow.augment(text, n=1)
+    
+                    if text != augmented_text:
+                        at_least_one_not_equal = True
+                        break
+    
+                self.assertTrue(at_least_one_not_equal)
+        self.assertLess(0, len(flows))
+        self.assertLess(0, len(texts))
+    
+    def test_n_output_textual(self):
+        texts = [
+            'The quick brown fox jumps over the lazy dog',
+            'Zology raku123456 fasdasd asd4123414 1234584',
+            'AAAAAAAAAAA AAAAAAAAAAAAAA'
+        ]
+        flows = [
+            naf.Sequential([
+                nac.RandomCharAug(action="insert"),
+                naw.RandomWordAug()
+            ]),
+            naf.Sometimes([
+                nac.RandomCharAug(action="insert"),
+                nac.RandomCharAug(action="delete")
+            ], pipeline_p=0.9),
+            naf.Sequential([
+                naf.Sequential([
+                    nac.RandomCharAug(action="insert"),
+                    naw.RandomWordAug()
+                ]),
+                naf.Sometimes([
+                    nac.RandomCharAug(action="insert"),
+                    nac.RandomCharAug(action="delete")
+                ], pipeline_p=0.9)
+            ])
+        ]
+    
+        for flow in flows:
+            for text in texts:
+                augmented_texts = flow.augment(text, n=3)
+                self.assertGreater(len(augmented_texts), 1)
+                for augmented_text in augmented_texts:
+                    self.assertNotEqual(augmented_text, text)
+    
+        self.assertLess(0, len(flows))
+        self.assertLess(0, len(texts))
+    
+    def test_n_output_audio(self):
+        audio, sampling_rate = AudioLoader.load_audio(self.sample_wav_file)
+    
+        flows = [
+            naf.Sequential([
+                naa.CropAug(sampling_rate=sampling_rate),
+                naa.LoudnessAug()
+            ]),
+            naf.Sometimes([
+                naa.CropAug(sampling_rate=sampling_rate),
+                naa.LoudnessAug()
+            ], pipeline_p=0.9),
+            naf.Sequential([
+                naf.Sequential([
+                    naa.CropAug(sampling_rate=sampling_rate),
+                    naa.LoudnessAug()
+                ]),
+                naf.Sometimes([
+                    naa.CropAug(sampling_rate=sampling_rate),
+                    naa.LoudnessAug()
+                ], pipeline_p=0.9)
+            ])
+        ]
+    
+        for flow in flows:
+            augmented_audios = flow.augment(audio, n=3)
+            self.assertGreater(len(augmented_audios), 1)
+            for augmented_audio in augmented_audios:
+                self.assertFalse(np.array_equal(audio, augmented_audio))
+    
+        self.assertLess(0, len(flows))
+    
+    def test_n_output_spectrogram(self):
+        mel_spectrogram = AudioLoader.load_mel_spectrogram(self.sample_wav_file, n_mels=128)
+    #
+        flows = [
+            naf.Sequential([
+                nas.FrequencyMaskingAug(),
+                nas.TimeMaskingAug()
+            ]),
+            naf.Sometimes([
+                nas.FrequencyMaskingAug(),
+                nas.TimeMaskingAug()
+            ], pipeline_p=0.9),
+            naf.Sequential([
+                naf.Sequential([
+                    nas.FrequencyMaskingAug(),
+                    nas.TimeMaskingAug()
+                ]),
+                naf.Sometimes([
+                    nas.FrequencyMaskingAug(),
+                    nas.TimeMaskingAug()
+                ], pipeline_p=0.9)
+            ])
+        ]
+    
+        for flow in flows:
+            augmented_mel_spectrograms = flow.augment(mel_spectrogram, n=3)
+            self.assertGreater(len(augmented_mel_spectrograms), 1)
+            for augmented_mel_spectrogram in augmented_mel_spectrograms:
+                self.assertFalse(np.array_equal(mel_spectrogram, augmented_mel_spectrogram))
+    
+        self.assertLess(0, len(flows))
+    
+    def test_n_output_without_augmentation(self):
+        texts = [
+            'AAAAAAAAAAA AAAAAAAAAAAAAA'
+        ]
+        flows = [
+            naf.Sequential([
+                nac.OcrAug(),
+                nac.OcrAug()
+            ]),
+            naf.Sometimes([
+                nac.RandomCharAug(),
+                nac.RandomCharAug()
+            ], pipeline_p=0.00001)
+        ]
+    
+        for flow in flows:
+            for text in texts:
+                for _ in range(5):
+                    augmented_texts = flow.augment(text, n=3)
+                    all_not_equal = False
+                    for augmented_text in augmented_texts:
+                        if augmented_text != text:
+                            all_not_equal = True
+                            break
+                    if all_not_equal:
+                        break
+    
+                self.assertFalse(all_not_equal)
+        self.assertLess(0, len(flows))
+        self.assertLess(0, len(texts))
+    
+    def test_multi_thread(self):
+        text = 'The quick brown fox jumps over the lazy dog'
+        n = 3
 
-            self.assertNotEqual(text, augmented_text)
-            self.assertGreater(len(augment_details), 0)
-            for augment_detail in augment_details:
-                self.assertGreater(augment_detail['orig_start_pos'], -1)
-                self.assertGreater(augment_detail['new_start_pos'], -1)
-                self.assertGreater(augment_detail['change_seq'], 0)
-                self.assertIn(augment_detail['action'], Action.getall())
+        w2v_model_path = os.path.join(os.environ["MODEL_DIR"], 'word', 'word_embs', 'GoogleNews-vectors-negative300.bin')
+
+        flows = [
+            naf.Sequential([
+                naf.Sequential([
+                    nac.OcrAug(),
+                    naw.WordEmbsAug(
+                        model_type='word2vec',
+                        model_path=w2v_model_path)
+                ]),
+                naf.Sequential([
+                    nac.RandomCharAug(),
+                ]),
+                naw.ContextualWordEmbsAug(
+                    model_path='xlnet-base-cased', action="substitute",
+                    temperature=0.7, device='cpu')
+            ]),
+            naf.Sometimes([
+                naf.Sequential([
+                    nac.OcrAug(),
+                    nac.RandomCharAug(),
+                ]),
+                naf.Sometimes([
+                    naw.WordEmbsAug(model_type='word2vec',
+                                    model_path=w2v_model_path)
+                ], pipeline_p=0.999),
+                naw.ContextualWordEmbsAug(
+                    model_path='xlnet-base-cased', action="substitute",
+                    temperature=0.7, device='cpu')
+            ], pipeline_p=0.9999)
+        ]
+    
+        for num_thread in [1, 3]:
+            for flow in flows:
+                augmented_data = flow.augment(text, n=n, num_thread=num_thread)
+                self.assertEqual(len(augmented_data), n)
+
+    # def test_augment_detail(self):
+    #     text = 'The quick brown fox jumps over the lazy dog'
+
+    #     flows = [
+    #         naf.Sequential([
+    #             naf.Sometimes([nac.RandomCharAug(action="insert"),
+    #                            nac.RandomCharAug(action="delete")],
+    #                           pipeline_p=0.5),
+    #             naf.Sequential([
+    #                 nac.RandomCharAug(action="substitute", aug_char_min=1, aug_char_p=0.6, aug_word_p=0.6)
+    #             ], name='Sub_Seq')
+    #         ], include_detail=True),
+    #         naf.Sometimes([
+    #             naf.Sometimes([nac.RandomCharAug(action="insert"),
+    #                            nac.RandomCharAug(action="delete")]),
+    #             naf.Sequential([nac.OcrAug(), nac.KeyboardAug(aug_char_min=1),
+    #                             nac.RandomCharAug(action="substitute", aug_char_min=1, aug_char_p=0.6, aug_word_p=0.6)])
+    #         ], pipeline_p=1, include_detail=True)
+    #     ]
+
+    #     for flow in flows:
+    #         augmented_text, augment_details = flow.augment(text)
+
+    #         self.assertNotEqual(text, augmented_text)
+    #         self.assertGreater(len(augment_details), 0)
+    #         for augment_detail in augment_details:
+    #             self.assertGreater(augment_detail['orig_start_pos'], -1)
+    #             self.assertGreater(augment_detail['new_start_pos'], -1)
+    #             self.assertGreater(augment_detail['change_seq'], 0)
+    #             self.assertIn(augment_detail['action'], Action.getall())
--- test/run_test.py
+++ test/run_test.py
@@ -6,6 +6,8 @@ import logging
 if __name__ == '__main__':
     sys.path.append('../nlpaug')
 
+    runner = unittest.TextTestRunner()
+
     # disable transformer's info logging
     for file_name in ['tokenization_utils', 'file_utils', 'modeling_utils', 'modeling_xlnet',
                       'configuration_utils']:
@@ -21,23 +23,53 @@ if __name__ == '__main__':
         'test/model/word/',
         'test/util/selection/',
         'test/flow/',
-        'test/profiling/sentence/',
+        # 'test/profiling/sentence/',
     ]
-    runner = unittest.TextTestRunner()
-
+    
     for test_dir in test_dirs:
        loader = unittest.TestLoader()
        suite = loader.discover(test_dir)
        runner.run(suite)
 
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.sentence.test_context_word_embs_sentence')
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.word.test_context_word_embs')
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.word.test_word_embs')
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.word.test_random_word')
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.char.test_random_char')
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.word.test_word')
-    # suite = unittest.TestLoader().loadTestsFromName('util.selection.test_filtering')
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.audio.test_noise')
-    # suite = unittest.TestLoader().loadTestsFromName('augmenter.test_augmenter')
-    # suite = unittest.TestLoader().loadTestsFromName('model.word.test_word_embs_model')
-    # runner.run(suite)
+    suites = []
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.test_augmenter'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('util.text.test_tokenizer'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('util.selection.test_filtering'))
+    
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.test_text_augmenter'))
+    
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.sentence.test_context_word_embs_sentence'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.sentence.test_abst_summ'))
+    
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_word'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_tfidf'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_spelling'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_antonym'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_synonym'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_context_word_embs'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_back_translation'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_word_embs'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.word.test_random_word'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('model.word.test_word_embs_model'))
+    
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.char.test_char'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.char.test_random_char'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.char.test_keyboard'))
+    
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.test_audio_augmenter'))
+
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_audio'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_crop'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_loudness'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_mask'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_noise'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_pitch'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_shift'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_speed'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('augmenter.audio.test_vtlp'))
+
+    # suites.append(unittest.TestLoader().loadTestsFromName('flow.test_flow'))
+    # suites.append(unittest.TestLoader().loadTestsFromName('flow.test_sequential'))
+
+    for suite in suites:
+        runner.run(suite)
