02da9fa8d14f9957231ee0f3adc3173dfc51fb20
Edward Ma <makcedward@gmail.com>
2020-09-18 22:36:08 -0700

    BackTranslation supports loading model from local directory #149

--- test/augmenter/word/test_back_translation.py
+++ test/augmenter/word/test_back_translation.py
@@ -28,39 +28,39 @@ class TestBackTranslationAug(unittest.TestCase):
             }
         ]
 
-    # def test_back_translation(self):
-    #     # From English
-    #     texts = [
-    #         self.text, 
-    #         "Seeing all of the negative reviews for this movie, I figured that it could be yet another comic masterpiece that wasn't quite meant to be."
-    #     ]
-    #     for model_name in self.eng_model_names:
-    #         aug = naw.BackTranslationAug(
-    #             from_model_name=model_name['from_model_name'], from_model_checkpt=model_name['from_model_checkpt'],
-    #             to_model_name=model_name['to_model_name'], to_model_checkpt=model_name['to_model_checkpt'])
-    #         augmented_text = aug.augment(self.text)
-    #         aug.clear_cache()
-    #         self.assertNotEqual(self.text, augmented_text)
+    def test_back_translation(self):
+        # From English
+        texts = [
+            self.text, 
+            "Seeing all of the negative reviews for this movie, I figured that it could be yet another comic masterpiece that wasn't quite meant to be."
+        ]
+        for model_name in self.eng_model_names:
+            aug = naw.BackTranslationAug(
+                from_model_name=model_name['from_model_name'], from_model_checkpt=model_name['from_model_checkpt'],
+                to_model_name=model_name['to_model_name'], to_model_checkpt=model_name['to_model_checkpt'])
+            augmented_text = aug.augment(self.text)
+            aug.clear_cache()
+            self.assertNotEqual(self.text, augmented_text)
 
-    #         augmented_texts = aug.augment(texts)
-    #         aug.clear_cache()
-    #         for d, a in zip(texts, augmented_texts):
-    #             self.assertNotEqual(d, a)        
+            augmented_texts = aug.augment(texts)
+            aug.clear_cache()
+            for d, a in zip(texts, augmented_texts):
+                self.assertNotEqual(d, a)        
 
-    #     self.assertTrue(len(self.eng_model_names) > 1)
+        self.assertTrue(len(self.eng_model_names) > 1)
 
-    # def test_load_from_local_path(self):
-    #     base_model_dir = os.environ.get("MODEL_DIR")
-    #     from_model_dir = os.path.join(base_model_dir, 'word', 'fairseq', 'wmt19.en-de')
-    #     to_model_dir = os.path.join(base_model_dir, 'word', 'fairseq', 'wmt19.de-en', '')
+    def test_load_from_local_path(self):
+        base_model_dir = os.environ.get("MODEL_DIR")
+        from_model_dir = os.path.join(base_model_dir, 'word', 'fairseq', 'wmt19.en-de')
+        to_model_dir = os.path.join(base_model_dir, 'word', 'fairseq', 'wmt19.de-en', '')
 
-    #     aug = naw.BackTranslationAug(
-    #         from_model_name=from_model_dir, from_model_checkpt='model1.pt',
-    #         to_model_name=to_model_dir, to_model_checkpt='model1.pt', is_load_from_github=False)
+        aug = naw.BackTranslationAug(
+            from_model_name=from_model_dir, from_model_checkpt='model1.pt',
+            to_model_name=to_model_dir, to_model_checkpt='model1.pt', is_load_from_github=False)
 
-    #     augmented_text = aug.augment(self.text)
-    #     aug.clear_cache()
-    #     self.assertNotEqual(self.text, augmented_text)
+        augmented_text = aug.augment(self.text)
+        aug.clear_cache()
+        self.assertNotEqual(self.text, augmented_text)
 
     def test_load_from_local_path_inexist(self):
         from_model_dir = '/abc/'
