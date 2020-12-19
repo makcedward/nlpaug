9e91dd1a0c49066e1bc194c3ed860260c2744bef
Edward Ma <makcedward@gmail.com>
2020-12-18 21:58:36 -0800

    PolarityInverseAug supports zone and coverage

--- nlpaug/augmenter/audio/inversion.py
+++ nlpaug/augmenter/audio/inversion.py
@@ -9,18 +9,29 @@ from nlpaug.util import Action, WarningMessage
 
 class PolarityInverseAug(AudioAugmenter):
     """
+    :param tuple zone: Assign a zone for augmentation. Default value is (0.2, 0.8) which means that no any
+        augmentation will be applied in first 20% and last 20% of whole audio.
+    :param float coverage: Portion of augmentation. Value should be between 0 and 1. If `0.1` is assigned, augment
+        operation will be applied to target audio segment. For example, the audio duration is 60 seconds while
+        zone and coverage are (0.2, 0.8) and 0.7 respectively. 25.2 seconds ((0.8-0.2)*0.7*60) audio will be
+        augmented.
     :param str name: Name of this augmenter
 
     >>> import nlpaug.augmenter.audio as naa
     >>> aug = naa.PolarityInverseAug()
     """
 
-    def __init__(self, name='PolarityInverse_Aug', verbose=0, stateless=True):
+    def __init__(self, zone=(0.2, 0.8), coverage=0.3, name='PolarityInverse_Aug', verbose=0, stateless=True):
         super().__init__(
-            action=Action.SUBSTITUTE, name=name, device='cpu', verbose=verbose, 
+            action=Action.SUBSTITUTE, zone=zone, coverage=coverage, name=name, device='cpu', verbose=verbose, 
             stateless=stateless)
 
         self.model = nma.PolarityInversion()
 
     def substitute(self, data):
-        return self.model.manipulate(data)
+        start_pos, end_pos = self.get_augment_range_by_coverage(data)
+        if not self.stateless:
+            self.start_pos = start_pos
+            self.end_pos = end_pos
+
+        return self.model.manipulate(data, start_pos=start_pos, end_pos=end_pos)
--- nlpaug/model/audio/inversion.py
+++ nlpaug/model/audio/inversion.py
@@ -4,5 +4,8 @@ from nlpaug.model.audio import Audio
 
 class PolarityInversion(Audio):
 	# https://en.wikipedia.org/wiki/Phase_inversion
-	def manipulate(self, data):
-		return data * -1
+	def manipulate(self, data, start_pos, end_pos):
+		aug_data = data.copy()
+		aug_data[start_pos:end_pos] = -aug_data[start_pos:end_pos]
+
+		return aug_data
