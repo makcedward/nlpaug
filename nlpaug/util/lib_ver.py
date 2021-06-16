721bad45be5d0c12d563debd2a381fd085d1c6c4
Edward Ma <makcedward@gmail.com>
2021-06-16 09:27:25 -0700

    add lib dependency and lib version util

---
+++ nlpaug/util/lib_ver.py
@@ -0,0 +1,36 @@
+import nlpaug
+from platform import python_version
+
+def get_lib_ver():
+	lib_ver = {
+		'python': python_version(),
+		'nlpaug': nlpaug.__version__
+	}
+
+	try:
+		import transformers
+		lib_ver['transformers'] = transformers.__version__
+	except:
+		pass
+
+	try:
+		import torch
+		lib_ver['torch'] = torch.__version__
+	except:
+		pass
+
+	try:
+		import fairseq
+		lib_ver['fairseq'] = fairseq.__version__
+	except:
+		pass
+
+	try:
+		import nltk
+		lib_ver['nltk'] = nltk.__version__
+	except:
+		pass
+
+	return lib_ver
+
+
