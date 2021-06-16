627f8c540c604e6f016f1cfa6c962d5be4a4b62a
Edward Ma <makcedward@gmail.com>
2021-06-15 22:07:14 -0700

    set seed

--- nlpaug/util/selection/randomness.py
+++ nlpaug/util/selection/randomness.py
@@ -12,5 +12,9 @@ class Randomness:
     def seed(seed):
         random.seed(seed)
         np.random.seed(seed)
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
+        try:
+            torch.manual_seed(seed)
+            torch.cuda.manual_seed(seed)
+            torch.cuda.manual_seed_all(2021)
+        except:
+            pass
