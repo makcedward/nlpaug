9bca26e17641951e1c9d638271050e47d0df4e53
Edward Ma <makcedward@gmail.com>
2021-06-17 21:22:49 -0700

    add new transformers models

--- nlpaug/model/lang_models/__init__.py
+++ nlpaug/model/lang_models/__init__.py
@@ -8,4 +8,6 @@ from nlpaug.model.lang_models.roberta import *
 from nlpaug.model.lang_models.fairseq import *
 from nlpaug.model.lang_models.t5 import *
 from nlpaug.model.lang_models.bart import *
-from nlpaug.model.lang_models.transformers import *
+from nlpaug.model.lang_models.fill_mask_transformers import *
+from nlpaug.model.lang_models.machine_translation_transformers import *
+from nlpaug.model.lang_models.summarization_transformers import *
