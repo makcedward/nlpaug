67147339b456036df0699e219236358e194df4cb
Edward Ma <makcedward@gmail.com>
2021-12-22 14:55:30 -0800

    rollback AbstSummaryAug to custom transformer API for speed improvmenet

--- nlpaug/augmenter/sentence/abst_summ.py
+++ nlpaug/augmenter/sentence/abst_summ.py
@@ -11,7 +11,8 @@ from nlpaug.util import Action, Doc
 ABST_SUMM_MODELS = {}
 
 def init_abst_summ_model(model_path, tokenizer_path, device, force_reload=False,
-    min_length=20, max_length=50, batch_size=32, temperature=1.0, top_k=50, top_p=0.9):
+    min_length=20, max_length=50, batch_size=32, temperature=1.0, top_k=50, top_p=0.9, 
+    use_custom_api=True):
     global ABST_SUMM_MODELS
 
     model_name = '_'.join([os.path.basename(model_path), os.path.basename(tokenizer_path), str(device)])
@@ -24,9 +25,22 @@ def init_abst_summ_model(model_path, tokenizer_path, device, force_reload=False,
         ABST_SUMM_MODELS[model_name].batch_size = batch_size
         return ABST_SUMM_MODELS[model_name]
 
-    model = nml.XSumTransformers(model_name=model_path, tokenizer_name=tokenizer_path, 
-        min_length=min_length, max_length=max_length, temperature=temperature, top_k=top_k,
-        top_p=top_p, batch_size=batch_size, device=device)
+    if use_custom_api:
+        num_beam = 3
+        no_repeat_ngram_size = 3
+        
+        if 't5' in model_path:
+            model = nml.T5(model_path, device=device, min_length=min_length, max_length=max_length,
+                num_beam=num_beam, no_repeat_ngram_size=no_repeat_ngram_size)
+        elif 'bart-large-cnn' in model_path:
+            model = nml.Bart(model_path, device=device, min_length=min_length, max_length=max_length,
+                num_beam=num_beam, no_repeat_ngram_size=no_repeat_ngram_size)
+        else:
+            raise ValueError('Model name value is unexpected. Only support `T5` and `bart-large-cnn` model.')
+    else:
+        model = nml.XSumTransformers(model_name=model_path, tokenizer_name=tokenizer_path, 
+            min_length=min_length, max_length=max_length, temperature=temperature, top_k=top_k,
+            top_p=top_p, batch_size=batch_size, device=device)
 
     ABST_SUMM_MODELS[model_name] = model
     return model
@@ -57,7 +71,7 @@ class AbstSummAug(SentenceAugmenter):
 
     def __init__(self, model_path='t5-base', tokenizer_path='t5-base', 
         min_length=20, max_length=50, batch_size=32, temperature=1.0, top_k=50, top_p=0.9, 
-        name='AbstSumm_Aug', device='cpu', force_reload=False, verbose=0):
+        name='AbstSumm_Aug', device='cpu', force_reload=False, verbose=0, use_custom_api=True):
         super().__init__(
             action=Action.SUBSTITUTE, name=name, tokenizer=None, stopwords=None, device=device,
             include_detail=False, verbose=verbose)
@@ -67,7 +81,7 @@ class AbstSummAug(SentenceAugmenter):
         self.model = self.get_model(
             model_path=model_path, tokenizer_path=tokenizer_path, device=device, force_reload=force_reload, 
             min_length=min_length, max_length=max_length, batch_size=batch_size, temperature=temperature,
-            top_k=top_k, top_p=top_p)
+            top_k=top_k, top_p=top_p, use_custom_api=use_custom_api)
         self.device = self.model.device
 
     def substitute(self, data):
@@ -85,6 +99,7 @@ class AbstSummAug(SentenceAugmenter):
 
     @classmethod
     def get_model(cls, model_path, tokenizer_path, device='cuda', force_reload=False, 
-        min_length=20, max_length=50, batch_size=32, temperature=1.0, top_k=50, top_p=0.9):
+        min_length=20, max_length=50, batch_size=32, temperature=1.0, top_k=50, top_p=0.9, 
+        use_custom_api=True):
         return init_abst_summ_model(model_path, tokenizer_path, device, force_reload, 
-            min_length, max_length, batch_size, temperature, top_k, top_p)
+            min_length, max_length, batch_size, temperature, top_k, top_p, use_custom_api)
