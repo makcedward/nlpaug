15ec525f3800ad501e087302c96c093ad2d97161
Edward Ma <makcedward@gmail.com>
2021-12-22 17:50:41 -0800

    rollback ContextualWordEmbsForSentenceAug to custom transformer API for speed improvmenet

--- nlpaug/augmenter/sentence/context_word_embs_sentence.py
+++ nlpaug/augmenter/sentence/context_word_embs_sentence.py
@@ -12,7 +12,7 @@ import nlpaug.util.text.tokenizer as text_tokenizer
 CONTEXT_WORD_EMBS_SENTENCE_MODELS = {}
 
 
-def init_context_word_embs_sentence_model(model_path, device, force_reload=False, 
+def init_context_word_embs_sentence_model(model_path, model_type, device, force_reload=False, 
     min_length=100, max_length=300, batch_size=32, temperature=1.0, top_k=50, top_p=0.9, 
     silence=True, use_custom_api=True):
 
@@ -31,11 +31,11 @@ def init_context_word_embs_sentence_model(model_path, device, force_reload=False
 
     if use_custom_api:
         if model_type == 'xlnet':
-            model = nml.XlNet(model_path, device=device, temperature=temperature, top_k=top_k, top_p=top_p,
-                              optimize=optimize, silence=True)
+            model = nml.XlNet(model_path, device=device, temperature=temperature, top_k=top_k, 
+                              optimize=None, silence=True)
         elif model_type == 'gpt2':
-            model = nml.Gpt2(model_path, device=device, temperature=temperature, top_k=top_k, top_p=top_p,
-                             optimize=optimize, silence=True)
+            model = nml.Gpt2(model_path, device=device, temperature=temperature, top_k=top_k, 
+                             optimize=None, silence=True)
         else:
             raise ValueError('Model name value is unexpected. Only support XLNet and GPT2 model.')
     else:
@@ -53,6 +53,8 @@ class ContextualWordEmbsForSentenceAug(SentenceAugmenter):
 
     :param str model_path: Model name or model path. It used transformers to load the model. Tested
         'gpt2', 'distilgpt2'. 
+    :param str model_type: Type of model. For XLNet model, use 'xlnet'. For GPT2 or distilgpt2 model, use 'gpt'. If 
+        no value is provided, will determine from model name.
     :param int batch_size: Batch size.
     :param int min_length: The min length of output text.
     :param int max_length: The max length of output text.
@@ -72,16 +74,19 @@ class ContextualWordEmbsForSentenceAug(SentenceAugmenter):
     >>> aug = nas.ContextualWordEmbsForSentenceAug()
     """
 
-    def __init__(self, model_path='gpt2', name='ContextualWordEmbsForSentence_Aug',
+    def __init__(self, model_path='gpt2', model_type='', name='ContextualWordEmbsForSentence_Aug',
         min_length=100, max_length=500, batch_size=32, temperature=1.0, top_k=50, top_p=0.9, 
         device='cpu', force_reload=False, silence=True, use_custom_api=True):
         super().__init__(
             action=Action.INSERT, name=name, tokenizer=None, stopwords=None, device=device,
             include_detail=False)
         self.model_path = model_path
+        self.model_type = model_type if model_type != '' else self.check_model_type() 
+        self.use_custom_api = use_custom_api
 
         self.model = self.get_model(
-            model_path=model_path, device=device, force_reload=force_reload, batch_size=batch_size,
+            model_path=model_path, model_type=self.model_type,
+            device=device, force_reload=force_reload, batch_size=batch_size,
             min_length=min_length, max_length=max_length, temperature=temperature,
             top_k=top_k, top_p=top_p, silence=silence, use_custom_api=use_custom_api)
         self.device = self.model.device
@@ -105,13 +110,94 @@ class ContextualWordEmbsForSentenceAug(SentenceAugmenter):
 
             all_data = [data]
 
+        if self.use_custom_api:
+            return self._custom_insert(all_data)
+        else:
+            return self._native_insert(all_data)
+
+    def _custom_insert(self, all_data):
+        max_try = 30  # On average 30 should be enough to complete a sentence
+        external_memories = [None] * len(all_data)
+        augmented_texts = [''] * len(all_data)
+        docs = [Doc()] * len(all_data)
+        early_stops = [0] * len(all_data)
+        change_seq = 0
+        aug_idx = 0
+
+        for _ in range(max_try):
+            if sum(early_stops) == len(all_data):
+                break
+
+            aug_input_poses = [] # store which input augmented. No augmentation if genrated a sentence
+            texts = []
+            for i, d in enumerate(all_data):
+                if early_stops[i] == 1:
+                    continue
+
+                aug_input_poses.append(i)
+                augmented_text = augmented_texts[i]
+                external_memory = external_memories[i]
+
+                if external_memory is None:  # First step or does not enable optimization
+                    text = d + augmented_text
+                else:
+                    text = ''
+
+                # Mask token is needed for xlnet. No mask token for gpt2
+                if self.model_type in ['xlnet']:
+                    text += ' ' + self.model.MASK_TOKEN
+
+                texts.append(text)
+
+            outputs = self.model.predict(texts, n=1, external_memory=external_memory, 
+                include_punctuation=True)
+
+            for i, output in enumerate(outputs):
+                aug_input_pos = aug_input_poses[i]
+
+                # TODO:
+                # if self.model.optimize['external_memory']:
+                #     external_memory = outputs[1]
+
+                # TODO: Alternative method better than dropout
+                candidate = ''
+                if len(output) == 1:
+                    candidate = output[0]
+                elif len(output) > 1:
+                    candidate = self.sample(output, 1)[0]
+
+                change_seq += 1
+                docs[aug_input_pos].add_token(aug_idx, token='', action=Action.INSERT, change_seq=0)
+                docs[aug_input_pos].update_change_log(aug_idx, token=self.model.clean(candidate), action=Action.INSERT,
+                    change_seq=self.parent_change_seq + change_seq)
+                aug_idx += 1
+
+                # early stop if all input generated a sentence.
+                if candidate in text_tokenizer.SENTENCE_SEPARATOR:
+                    if self.model_type in ['gpt2']:
+                        augmented_texts[aug_input_pos] += ' '
+                    augmented_texts[aug_input_pos] += candidate
+                    early_stops[aug_input_pos] = 1
+                else:
+                    if self.model_type in ['gpt2']:
+                        augmented_texts[aug_input_pos] += ' '
+                    augmented_texts[aug_input_pos] += candidate
+
+        if self.model_type in ['gpt2']:
+            results = [d + a for d, a in zip(all_data, augmented_texts)]
+        elif self.model_type in ['xlnet']:
+            results = [d + ' ' + self.model.tokenizer.convert_tokens_to_string(a) for d, a in zip(all_data, augmented_texts)]
+
+        return results
+
+    def _native_insert(self, all_data):
         return self.model.predict(all_data)
 
     @classmethod
-    def get_model(cls, model_path, device='cuda', force_reload=False, min_length=100, 
+    def get_model(cls, model_path, model_type, device='cuda', force_reload=False, min_length=100, 
         max_length=300, batch_size=32, temperature=1.0, top_k=50, top_p=0.9, silence=True, 
         use_custom_api=True):
-        return init_context_word_embs_sentence_model(model_path, device, force_reload, 
+        return init_context_word_embs_sentence_model(model_path, model_type, device, force_reload, 
             batch_size=batch_size, min_length=min_length, max_length=max_length, 
             temperature=temperature, top_k=top_k, top_p=top_p, silence=silence, 
             use_custom_api=use_custom_api)
